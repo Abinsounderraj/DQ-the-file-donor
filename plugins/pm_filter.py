dcee13b225bad462cb32e4bd39b4e6e2afccf7ee
Daneil-k <158211667+Daneil-k@users.noreply.github.com>
2024-02-17 12:13:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -38,6 +38,7 @@ BUTTONS = {}
 SPELL_CHECK = {}
 
 
+@Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     if message.chat.id != SUPPORT_CHAT_ID:
